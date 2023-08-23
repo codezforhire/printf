896e62fe6b385c620051528b50e6b8cfb6dc0d3e
Notyasser <showhad55@gmail.com>
2023-08-23 16:00:45 -0700

    FILES

--- main.h
+++ main.h
@@ -7,13 +7,11 @@
 #define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
 
-
 #define F_MINUS 1
 #define F_PLUS 2
 #define F_ZERO 4
 #define F_HASH 8
 #define F_SPACE 16
-
 
 #define S_LONG 2
 #define S_SHORT 1
@@ -44,6 +42,7 @@ int handle_print(const char *fmt, int *i,
 		va_list list, char buffer[], int flags, int width, int precision, int size);
 
 
+/* Funtions to print chars and strings */
 
 int print_char(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
@@ -52,6 +51,7 @@ int print_string(va_list types, char buffer[],
 int print_percent(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
 
+/* Functions to print numbers */
 
 int print_int(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
@@ -70,28 +70,34 @@ int print_hexa(va_list types, char map_to[],
 		char buffer[], int flags, char flag_ch, int width, int precision, int size);
 
 /* Function to print non printable characters */
+
 int print_non_printable(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
 
 /* Funcion to print memory address */
+
 int print_pointer(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
 
 /* Funciotns to handle other specifiers */
+
 int get_flags(const char *format, int *i);
 int get_width(const char *format, int *i, va_list list);
 int get_precision(const char *format, int *i, va_list list);
 int get_size(const char *format, int *i);
 
 /*Function to print string in reverse*/
+
 int print_reverse(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
 
 /*Function to print a string in rot 13*/
+
 int print_rot13string(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
 
 /* width handler */
+
 int handle_write_char(char c, char buffer[],
 		int flags, int width, int precision, int size);
 int write_number(int is_positive, int ind, char buffer[],
@@ -105,7 +111,8 @@ int write_unsgnd(int is_negative, int ind,
 		char buffer[],
 		int flags, int width, int precision, int size);
 
-/*************** UTILS ***************/
+
+
 int is_printable(char);
 int append_hexa_code(char, char[], int);
 int is_digit(char);
