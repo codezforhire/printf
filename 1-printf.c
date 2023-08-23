69b956003148a53160672a7daa770804c315ead1
Notyasser <showhad55@gmail.com>
2023-08-23 16:08:24 -0700

    function that produces output according to a format

--- 1-printf.c
+++ 1-printf.c
@@ -1,60 +1,48 @@
 #include "main.h"
-#include <stdarg.h>
-#include <unistd.h>
 
-/**
- * _printf - Printf function
- * @format: format
- * Return: Printed characters count
- */
-int _printf(const char *format, ...)
-{
-	int printed_chars = 0;
+int _printf(const char *format, ...) {
 	va_list args;
-
-	if (format == NULL)
-		return (-1);
-
 	va_start(args, format);
 
-	while (*format)
-	{
-		if (*format != '%')
-		{
-			write(1, format, 1);
-			printed_chars++;
-		}
-		else
-		{
-			format++;
-			if (*format == 'c')
-			{
-				char c = va_arg(args, int);
-				write(1, &c, 1);
-				printed_chars++;
-			}
-			else if (*format == 's')
-			{
-				char *s = va_arg(args, char *);
-				if (s == NULL)
-					s = "(null)";
-				while (*s)
-				{
-					write(1, s, 1);
-					s++;
-					printed_chars++;
+	char buffer[BUFF_SIZE];  /* Buffer to hold the formatted output */
+	int count = 0;           /* To keep track of characters printed */
+
+	for (int i = 0; format[i] != '\0'; i++) {
+		if (format[i] == '%') {
+			i++;  /* Move past the '%' */
+
+			/* Get flags, width, precision, and size from format */
+			int flags = get_flags(format, &i);
+			int width = get_width(format, &i, args);
+			int precision = get_precision(format, &i, args);
+			int size = get_size(format, &i);
+
+			/* Find the corresponding function for the specifier */
+			int found = 0;
+			for (int j = 0; format_specifiers[j].fmt != '\0'; j++) {
+				if (format_specifiers[j].fmt == format[i]) {
+					count += format_specifiers[j].fn(args, buffer, flags, width, precision, size);
+					found = 1;
+					break;
 				}
 			}
-			else if (*format == '%')
-			{
-				write(1, "%", 1);
-				printed_chars++;
+
+			/* If no valid specifier is found, print the '%' character */
+			if (!found) {
+				putchar('%');
+				count++;
 			}
+		} else {
+			putchar(format[i]);
+			count++;
 		}
-		format++;
 	}
 
 	va_end(args);
+	return count;
+}
 
-	return printed_chars;
+int main() {
+	_printf("Hello, %s! This is a %c and a percent sign %%.\n", "User", 'C');
+	return 0;
 }
